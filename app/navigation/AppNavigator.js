ce9c0b2a9f32ac28053b6e1dbf692f478f00cc32
vishwajithmech217-collab <vishwajithmech217@gmail.com>
2026-01-11 19:55:18 +0530

    Update AppNavigator.js

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,1 +1,19 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import SplashScreen from "../screens/SplashScreen";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
