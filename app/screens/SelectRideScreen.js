bc2de912ef37a17a7eeda7fbce917ab7befcc04b
vishwajithmech217-collab <vishwajithmech217@gmail.com>
2026-01-12 10:06:59 +0530

    Update SelectRideScreen.js

--- app/screens/SelectRideScreen.js
+++ app/screens/SelectRideScreen.js
@@ -1,1 +1,13 @@
+import { calculateScore } from "../logic/scoreEngine";
+import { filterByType } from "../logic/filters";
 
+const filtered = filterByType(vehicles, selectedType);
+
+const ranked = filtered
+  .map(v => ({
+    vehicle: v,
+    score: calculateScore(v, user)
+  }))
+  .sort((a, b) => b.score.total - a.score.total);
+
+// ranked[0] = BEST MATCH 🏆
