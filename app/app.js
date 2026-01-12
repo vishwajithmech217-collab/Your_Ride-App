c7d64fb1d3e386aa0ac66ff0060112e1e6919fce
vishwajithmech217-collab <vishwajithmech217@gmail.com>
2026-01-12 11:19:41 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,5 +1,30 @@
-import AppNavigator from "./app/navigation/AppNavigator";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import KnowRideScreen from "./screens/KnowRideScreen";
+import ModelDetailsScreen from "./screens/ModelDetailsScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen
+          name="KnowRide"
+          component={KnowRideScreen}
+        />
+
+        <Stack.Screen
+          name="ModelDetails"
+          component={ModelDetailsScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
